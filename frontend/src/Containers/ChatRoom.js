035d00576f7dc77d92f188a4b62a5098f5f0ab18
stevenHuang0604 <68052750+stevenHuang0604@users.noreply.github.com>
2021-06-26 17:31:28 +0800

    displaystatus success when add item successfully

--- frontend/src/Containers/ChatRoom.js
+++ frontend/src/Containers/ChatRoom.js
@@ -16,7 +16,7 @@ const ChatRoom = ({ me, displayStatus, setSignedIn }) => {
   const [category, setCategory] = useState("Housing");
   const [dollar, setDollar] = useState(0);
   const [boxes, setBoxes] = useState([]);
-  const { items, startDate, sendItem,sendDeleteItem } = useChat(displayStatus);
+  const { items, startDate, sendItem, sendDeleteItem } = useChat(displayStatus);
   const [open, setOpen] = useState(false);
   useEffect(() => {
     if (!open) {
@@ -54,6 +54,10 @@ const ChatRoom = ({ me, displayStatus, setSignedIn }) => {
         }
       }
     }
+    displayStatus({
+      type: "success",
+      msg: "Successfully add item.",
+    });
     sendItem(me, today, item, category, dollar);
     console.log(items);
     setBoxes(newboxes);
@@ -71,17 +75,17 @@ const ChatRoom = ({ me, displayStatus, setSignedIn }) => {
     //     }
     //   }
     // }
-    for (let i in boxes){
+    for (let i in boxes) {
       // console.log(boxes[i])
       total += Number(boxes[i].dollar);
     }
     return total;
   };
-  const deleteItem = (e)=>{
-    console.log(e.target.id)
-    sendDeleteItem(e.target.id)
+  const deleteItem = (e) => {
+    console.log(e.target.id);
+    sendDeleteItem(e.target.id);
     startDate(me, today);
-  }
+  };
   const columns = [
     {
       title: "Item",
@@ -135,9 +139,27 @@ const ChatRoom = ({ me, displayStatus, setSignedIn }) => {
       title: "Delete",
       key: "delete",
       render: (key) => (
-        <Space size="middle"  id={key.key}  onClick={deleteItem}>
-          <button className="item-delete" id={key.key} style={{ fontSize: "10px", borderColor: "transparent", borderRadius: "50%" }}>
-          <svg id={key.key} viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path id={key.key} d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg>
+        <Space size="middle" id={key.key} onClick={deleteItem}>
+          <button
+            className="item-delete"
+            id={key.key}
+            style={{ fontSize: "10px", borderColor: "transparent", borderRadius: "50%" }}
+          >
+            <svg
+              id={key.key}
+              viewBox="64 64 896 896"
+              focusable="false"
+              data-icon="close"
+              width="1em"
+              height="1em"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                id={key.key}
+                d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"
+              ></path>
+            </svg>
           </button>
         </Space>
       ),
@@ -204,7 +226,7 @@ const ChatRoom = ({ me, displayStatus, setSignedIn }) => {
           <Option value="Utilities">Utilities</Option>
           <Option value="Insurance">Insurance</Option>
           <Option value="Medical">Medical</Option>
-          <Option value="Investing">Saving, Investing,  Debt Payments</Option>
+          <Option value="Investing">Saving, Investing, Debt Payments</Option>
           <Option value="Entertainment">Entertainment</Option>
           <Option value="Miscellaneous">Miscellaneous</Option>
         </Select>
